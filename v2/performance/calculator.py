b5166b12e8cfa6ae86af75c63be3bbbaa15e8cd1
johngalimi <galimijohn@gmail.com>
2020-02-09 17:25:26 -0500

    fast_git commit: Sun Feb  9 17:25:26 EST 2020

---
+++ v2/performance/calculator.py
@@ -0,0 +1,15 @@
+class Calculator:
+    def __init__(self):
+        self.portfolio = None
+        self.position = None
+        self.horizon_start = None
+        self.horizon_end = None
+
+    def get_return(self):
+        pass
+
+    def get_risk(self):
+        pass
+
+    def get_sharpe(self):
+        pass
--- v2/strategy/trade_timer.py
+++ v2/strategy/trade_timer.py
@@ -1,9 +1,11 @@
 class TradeTimer:
     def __init__(self):
         pass
+        # maybe z score threshold
 
     def identify_entry_point(self):
         pass
 
     def identify_exit_point(self):
         pass
+
