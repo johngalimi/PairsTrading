289797b181d2e84dd00c958f88a5337daf4f1111
johngalimi <galimijohn@gmail.com>
2020-02-09 17:03:10 -0500

    fast_git commit: Sun Feb  9 17:03:10 EST 2020

--- v2/models/order.py
+++ v2/models/order.py
@@ -2,12 +2,14 @@ from .base import Base
 
 class Order(Base):
     def __init__(self, buys, sells):
-        self.buys = {}
-        self.sells = {}
+        self.buys = buys
+        self.sells = sells
 
-    def get_orders(self):
-        order_array = {}
-        for buy in self.buys:
-            pass
-        for sell in self.sells:
-            pass
+    def generate_order(self):
+        orders = {'BUY':[], 'SELL':[]}
+        for ticker in self.buys:
+            orders['BUY'].append((ticker, self.buys[ticker]))
+        for ticker in self.sells:
+            orders['SELL'].append((ticker, self.sells[ticker]))
+
+        return orders
--- v2/pm.py
+++ v2/pm.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from models.account import Account
+from models.order import Order
 
 if __name__ == '__main__':
     
@@ -14,4 +15,16 @@ if __name__ == '__main__':
     acct2.execute_transaction('SELL', 'AAPL', 5)
     acct2.fund_account(5000)
     acct2.view_portfolio()
+
+    target_buys = {'W':85, 'AAPL': 126}
+    target_sells = {'W': 17, 'AAPL': 29}
+
+    order = Order(target_buys, target_sells)
+    consolidated_orders = order.generate_order()
+    
+    for order_type in consolidated_orders:
+        for ticker, quantity in consolidated_orders[order_type]:
+            acct2.execute_transaction(order_type, ticker, quantity)
+
+
 
