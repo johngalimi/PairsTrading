c2e12c6e5239dd4b0762a3a1ced65d2ce3a5371f
johngalimi <galimijohn@gmail.com>
2020-08-09 15:33:15 -0400

    fast_git commit: Sun Aug  9 15:33:15 EDT 2020

--- v3/execution_server.go
+++ v3/execution_server.go
@@ -1,47 +1,54 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
-	"time"
 	"net/http"
-	// "encoding/json"
+	"time"
 	"github.com/gorilla/mux"
 )
 
+// Position - Representation of Single Stock
 type Position struct {
-	ticker string 
-	purchasePrice float32
-	purchaseQuantity int64
-	purchaseDatetime time.Time
+	Ticker           string    `json:"ticker"`
+	PurchasePrice    float32   `json:"purchase_price"`
+	PurchaseQuantity int64     `json:"purchase_Quantity"`
+	PurchaseDate     time.Time `json:"purchase_date"`
 }
 
+// Positions - Representation of Collection of Stocks (Portfolio)
+var Positions []Position
+
+func getPositions(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(Positions)
+}
 
 func liveness(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "Execution Server: Live\n")
 }
 
-func headers(w http.ResponseWriter, req *http.Request) {
-
-	for name, headers := range req.Header {
-		for _, h := range headers {
-			fmt.Fprintf(w, "%v: %v\n", name, h)
-		}
-	}
-}
-
 func handleRequests() {
 
-	fmt.Println(time.Now(), "Execution Server: Ready")
+	fmt.Println("Server Started: ", time.Now(), "|| Listening on :8090", "|| Execution Server: Ready")
 
 	myRouter := mux.NewRouter().StrictSlash(true)
 
-	myRouter.HandleFunc("/liveness", liveness)
-	myRouter.HandleFunc("/headers", headers)
+	myRouter.HandleFunc("/", liveness).Methods("GET")
+	myRouter.HandleFunc("/positions", getPositions).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8090", myRouter))
 }
 
 func main() {
+	Positions = []Position{
+		Position{
+			Ticker: "AAPL", PurchasePrice: 200.50, PurchaseQuantity: 5, PurchaseDate: time.Now()
+		},
+		Position{
+			Ticker: "F", PurchasePrice: 41.75, PurchaseQuantity: 3, PurchaseDate: time.Now().Add(time.Duration(100))
+		},
+	}
+
 	handleRequests()
 }
