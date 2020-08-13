cec0ccc0faa112a4f3d2bc2480aa8f217da3e4db
johngalimi <galimijohn@gmail.com>
2020-08-12 20:48:00 -0400

    url construction helper

--- v3/trading_client.py
+++ v3/trading_client.py
@@ -1,20 +1,25 @@
 import requests
 
 HOSTNAME_ROOT = "http://localhost:8090"
-POST_HEADERS = {'Content-type': 'application/json'}
+POST_HEADERS = {"Content-type": "application/json"}
 
 TRANSACTIONS_ENDPOINT = "/transactions"
 POSITIONS_ENDPOINT = "/positions"
 TRANSACTION_ENDPOINT = "/transaction"
 
+
 class TradingClient:
+    def _construct_url(self, endpoint):
+        return f"{HOSTNAME_ROOT}{endpoint}"
 
     def _get_helper(self, endpoint):
-        response = requests.get(f"{HOSTNAME_ROOT}{endpoint}")
+        response = requests.get(self._construct_url(endpoint))
         return response.json()
 
     def _post_helper(self, endpoint, data):
-        response = requests.post(f"{HOSTNAME_ROOT}{endpoint}", headers=POST_HEADERS, json=data)
+        response = requests.post(
+            self._construct_url(endpoint), headers=POST_HEADERS, json=data
+        )
         return response.json()
 
     def get_transactions(self):
@@ -24,36 +29,36 @@ class TradingClient:
         return self._get_helper(endpoint=POSITIONS_ENDPOINT)
 
     def execute_transaction(self, ticker, price, quantity):
-        transaction_object = {'ticker': ticker, 'price': price, 'quantity': quantity}
+        transaction_object = {"ticker": ticker, "price": price, "quantity": quantity}
         return self._post_helper(endpoint=TRANSACTION_ENDPOINT, data=transaction_object)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     trader = TradingClient()
 
     transactions = trader.get_transactions()
-    print('Transactions:', transactions)
+    print("Transactions:", transactions)
 
     print("\n")
 
     positions = trader.get_positions()
-    print('Positions:', positions)
+    print("Positions:", positions)
 
     print("\n")
 
-    transaction = trader.execute_transaction(ticker='FB', price=182.76, quantity=12)
-    transaction = trader.execute_transaction(ticker='FB', price=190.1, quantity=-8)
-    transaction = trader.execute_transaction(ticker='F', price=50, quantity=8)
-    transaction = trader.execute_transaction(ticker='AAPL', price=221.23, quantity=-3)
+    transaction = trader.execute_transaction(ticker="FB", price=182.76, quantity=12)
+    transaction = trader.execute_transaction(ticker="FB", price=190.1, quantity=-8)
+    transaction = trader.execute_transaction(ticker="F", price=50, quantity=8)
+    transaction = trader.execute_transaction(ticker="AAPL", price=221.23, quantity=-3)
 
     print("\n")
 
     transactions = trader.get_transactions()
-    print('Transactions:', transactions)
+    print("Transactions:", transactions)
 
     print("\n")
 
     positions = trader.get_positions()
-    print('Positions:', positions)
+    print("Positions:", positions)
 
     print("\n")
