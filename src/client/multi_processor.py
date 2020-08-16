25da53f4c83cd6ef5b732518a9daa54453607b26
johngalimi <galimijohn@gmail.com>
2020-08-16 08:36:15 -0400

    fast_git commit: Sun Aug 16 08:36:15 EDT 2020

--- src/client/multi_processor.py
+++ src/client/multi_processor.py
@@ -1,8 +1,6 @@
 import time
 import multiprocessing
 
-prime_numbers = []
-
 
 def is_prime(n):
     if n <= 1:
@@ -23,28 +21,23 @@ def is_prime(n):
     return True
 
 
-def test_single_digit(number):
+def check_single_digit(number, prime_array):
     time.sleep(2)
-    if is_prime(number):
-        prime_numbers.append(number)
+    prime_array.append({number: is_prime(number)})
 
 
 if __name__ == "__main__":
-    start = time.time()
-
-    for number in range(1, 10):
-        test_single_digit(number)
-
-    print(prime_numbers)
-
-    # ~18s on average
-    print(f"Time Elapsed: {time.time() - start}")
 
     start = time.time()
     processes = []
 
+    manager = multiprocessing.Manager()
+    prime_numbers = manager.list()
+
     for number in range(1, 10):
-        p = multiprocessing.Process(target=test_single_digit, args=(number,))
+        p = multiprocessing.Process(
+            target=check_single_digit, args=(number, prime_numbers)
+        )
         processes.append(p)
         p.start()
 
@@ -52,6 +45,4 @@ if __name__ == "__main__":
         process.join()
 
     print(prime_numbers)
-
-    # ~2s on average
     print(f"Time Elapsed: {time.time() - start}")
